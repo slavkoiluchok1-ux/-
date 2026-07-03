025544844678d80604c64a366218e7fafefc39bc
slavkoiluchok1-ux <slavkoiluchok1@gmail.com>
2026-07-03 19:04:45 +0300

    Add files via upload

--- trchnoblog/blog/admin.py
+++ trchnoblog/blog/admin.py
@@ -1,3 +1,58 @@
-from django.contrib import admin
+import csv
 
-# Register your models here.
+from django.contrib import admin
+from django.http import HttpResponse
+
+from .models import Article, Tag
+
+
+@admin.action(description='Mark selected articles as featured')
+def make_featured(modeladmin, request, queryset):
+    queryset.update(is_featured=True)
+
+
+@admin.action(description='Reset views counter for selected articles')
+def reset_views(modeladmin, request, queryset):
+    queryset.update(views=0)
+
+
+@admin.action(description='Export selected articles to CSV')
+def export_as_csv(modeladmin, request, queryset):
+    field_names = ['title', 'author', 'views', 'likes', 'is_featured', 'published_at']
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=articles.csv'
+
+    writer = csv.writer(response)
+    writer.writerow(field_names)
+
+    for article in queryset:
+        writer.writerow([
+            article.title,
+            article.author,
+            article.views,
+            article.likes,
+            article.is_featured,
+            article.published_at.isoformat(),
+        ])
+
+    return response
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'views', 'likes', 'is_featured', 'published_at']
+    list_filter = ['is_featured', 'published_at', 'author', 'tags']
+    search_fields = ['title', 'content']
+    list_editable = ['is_featured']
+    filter_horizontal = ['tags']
+    readonly_fields = ['views', 'likes']
+    actions = [make_featured, reset_views, export_as_csv]
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'color']
+    search_fields = ['name']
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Tag, TagAdmin)
--- trchnoblog/blog/models.py
+++ trchnoblog/blog/models.py
@@ -1,3 +1,27 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    color = models.CharField(max_length=7, default='#007bff')
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, blank=True)
+    is_featured = models.BooleanField(default=False)
+    views = models.PositiveIntegerField(default=0)
+    likes = models.PositiveIntegerField(default=0)
+    published_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-published_at']
+
+    def __str__(self) -> str:
+        return self.title
